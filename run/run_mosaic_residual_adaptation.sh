1a6d108d444eb8a18d0d546316615b95e8aee01b
Zhenguo Sun <hitsunzhenguo@163.com>
2026-02-12 02:00:41 +0800

    initial commit

--- run/run_mosaic_residual_adaptation.sh
+++ run/run_mosaic_residual_adaptation.sh
@@ -1,6 +1,6 @@
 # 3rd stage
 # MOSAIC residual adaptation
-HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 --nproc_per_node=6 scripts/rsl_rl/train.py \
+HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 --nproc_per_node=8 scripts/rsl_rl/train.py \
     --task=MOSAIC-MultiTeacher-Residual-Tracking-Flat-G1-v0 \
     --distributed \
     --num_envs=24000 \
